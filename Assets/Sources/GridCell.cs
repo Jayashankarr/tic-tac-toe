b0f0a98a2f655909e5b23f8cdee565743f0a007b
jayashankar <jayasankarrnair@gmail.com>
2020-03-15 09:46:57 +0530

    Added basic cell button logic

--- Assets/Sources/GridCell.cs
+++ Assets/Sources/GridCell.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
+//using static System.Net.Mime.MediaTypeNames;
 
 public class GridCell : MonoBehaviour
 {
+    [SerializeField]
+    private Image x;
+
+    [SerializeField]
+    private Image o;
+
+    [SerializeField]
+    private Button button = null;
+
+    [SerializeField]
+    private string playerTxt = null;
+
+    [SerializeField]
+    private Text buttonText = null;
+
+    public void setCell ()
+    {
+        Debug.Log ("button pressed");
+        buttonText.text = playerTxt;
+        button.interactable = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
